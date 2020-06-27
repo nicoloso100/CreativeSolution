436dc9da28bafd3acc6e389ab3be92fc758d9248
nicoloso100 <nico.las0315@hotmail.com>
2020-06-27 18:18:43 -0500

    page templateÃ

---
+++ api/src/index.ts
@@ -0,0 +1,9 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.use("/", express.static("../page/"));
+
+app.listen(port, () => {
+  console.log("Server started on port ", port);
+});
